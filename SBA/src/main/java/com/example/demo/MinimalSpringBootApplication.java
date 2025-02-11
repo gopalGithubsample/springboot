c149c00bb5491283a33910e5b2f7c0a16aa857ab
Gopal <139344893+gopalGithubsample@users.noreply.github.com>
2025-02-11 12:29:27 +0530

    commit
    Day 7 & 8

---
+++ SBA/src/main/java/com/example/demo/MinimalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinimalSpringBootApplication.class, args);
+    }
+
+}

79e11bb55d83d9eeebb00b1d93cd8b54cc5777ea
Gopal <139344893+gopalGithubsample@users.noreply.github.com>
2025-02-11 17:44:47 +0530

    commit
    Day 7 & 8

---
+++ Tasks2/src/main/java/com/example/demo/Tasks2Application.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tasks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tasks2Application.class, args);
+		
+		System.out.println("My Name is this");
+	//	System.out.println("My Name is That");
+	}
+
+}
+
+
